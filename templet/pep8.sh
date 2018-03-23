cd44167884b809503d3ca9ad983d768c21c66359
Brightcells <brightcells@gmail.com>
2018-03-23 17:41:56 +0800

    :art: Ignore Pep8 E402

--- templet/pep8.sh
+++ templet/pep8.sh
@@ -4,6 +4,7 @@
 #  -- Migration directories
 # Ignoring error codes
 #  -- E128 continuation line under-indented for visual indent
+#  -- E402 module level import not at top of file
 #  -- E501 line too long
 
-pycodestyle --exclude=build,migrations,.tox --ignore=E128,E501 .
+pycodestyle --exclude=build,migrations,.tox --ignore=E128,E402,E501 .

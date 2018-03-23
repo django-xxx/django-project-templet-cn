b41180bd8fff26fa49ddd11d068be17ca4af8230
Brightcells <brightcells@gmail.com>
2018-03-23 17:25:09 +0800

    :art: Support JsonP Response

--- templet/utils/error/response_utils.py
+++ templet/utils/error/response_utils.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.http import JsonResponse
+from json_response import JsonpResponse, JsonResponse
 from StatusCode import StatusCodeField
 
 
@@ -13,6 +13,12 @@ def response_data(status_code=200, message=None, description=None, data={}, **kw
     }, **kwargs)
 
 
-def response(status_code=200, message=None, description=None, data={}, **kwargs):
+def response(status_code=200, message=None, description=None, data={}, msg_args=[], msg_kwargs={}, desc_args=[], desc_kwargs={}, request=None, callback=None, **kwargs):
+    # Final Message and Description
     message, description = (message or status_code.message, description or status_code.description) if isinstance(status_code, StatusCodeField) else (message, description)
-    return JsonResponse(response_data(status_code, message, description, data, **kwargs), safe=False)
+    # Final Response Data
+    resp_data = response_data(status_code, (message or '').format(*msg_args, **msg_kwargs), (description or '').format(*desc_args, **desc_kwargs), data, **kwargs)
+    # Assign Callback
+    callback = callback or (request and request.GET.get('callback'))
+    # Call JsonResponse or JsonpResponse
+    return JsonpResponse(callback, resp_data, safe=False) if callback else JsonResponse(resp_data, safe=False)

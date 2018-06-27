4fd4f396b5fc61106cb7c627d8e9d87a1f3b7156
Brightcells <brightcells@gmail.com>
2018-06-27 15:18:43 +0800

    :art: Update command cmd_bak

--- templet/commands/management/commands/cmd_bak.py
+++ templet/commands/management/commands/cmd_bak.py
@@ -17,6 +17,10 @@ class Command(CompatibilityBaseCommand):
         logger.info('Templet bak cmd is dealing')
 
         while True:
+            # r.rpush('TEMPLET_CMD_KEY', '')
+            # kv = r.blpop('TEMPLET_CMD_KEY', 60)
+            # k, v = (kv[0], kv[1]) if kv else (None, None)
+            #
             # r.rpushjson('TEMPLET_CMD_KEY', {})
             k, v = r.blpopjson('TEMPLET_CMD_KEY', 60)
 

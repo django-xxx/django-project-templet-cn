deea3c019cb681281e338548c29bfdd3f2632c14
huangqimin001 <huangqimin001@ke.com>
2020-11-30 02:04:34 +0800

    :art: Remove db_index=True for BooleanField status

--- templet/templet/basemodels.py
+++ templet/templet/basemodels.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class BaseModelMixin(models.Model):
-    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'状态'), db_index=True)
+    status = models.BooleanField(_(u'status'), default=True, help_text=_(u'状态'))
     created_at = models.DateTimeField(_(u'created_at'), auto_now_add=True, editable=True, help_text=_(u'创建时间'))
     updated_at = models.DateTimeField(_(u'updated_at'), auto_now=True, editable=True, help_text=_(u'更新时间'))
 
